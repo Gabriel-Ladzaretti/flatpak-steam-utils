424802e7d7b6c6327145695a0ea9acd5e55479f3
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2025-01-01 01:34:28 +0200

    configurable hicolor dest dir

--- icon_resize.sh
+++ icon_resize.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 
 source_icon=${1:?"Error: Argument <icon_source> is required. Usage: $0 <icon_source> <output_icon_name>"}
 output_icon_name=${2:?"Error: Argument <output_icon_name> is required. Usage: $0 <icon_source> <output_icon_name>"}
-
-USER_HICOLOR_DIR="${HOME}/.local/share/icons/hicolor"
+dest_hicolor_dir="${3:-${HOME}/.local/share/icons/hicolor}"
 
 if ! command -v magick &>/dev/null; then
     echo "Error: 'magick' command not found. Please install ImageMagick." >&2
@@ -29,8 +28,8 @@ sizes=(
 )
 
 for size in "${sizes[@]}"; do
-    echo magick "$source_icon" -resize "$size" "${USER_HICOLOR_DIR}/${size}/apps/${output_icon_name}" &&
-        magick "$source_icon" -resize "$size" "${USER_HICOLOR_DIR}/${size}/apps/${output_icon_name}"
+    echo magick "$source_icon" -resize "$size" "${dest_hicolor_dir}/${size}/apps/${output_icon_name}" &&
+        magick "$source_icon" -resize "$size" "${dest_hicolor_dir}/${size}/apps/${output_icon_name}"
 done
 
 sudo gtk-update-icon-cache -f /usr/share/icons/hicolor/
